e793fffc71bae562815bf05efca33be9aa7b2293
Virtual1 <jessica@virtualangels.co.za>
2018-11-27 16:28:09 +0200

    Django installed
    Django installed

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jessica\documents\github\grip-project-001\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
